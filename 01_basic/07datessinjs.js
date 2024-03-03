72b539ac90313252e41891a878daf6d12eea8ff4
sandeep192001 <107060962+sandeep192001@users.noreply.github.com>
2024-03-03 10:43:45 +0000

    date and time

---
+++ 01_basic/07datessinjs.js
@@ -0,0 +1,10 @@
+let myDate=new Date();
+// console.log(myDate);
+
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toString());
+// console.log(typeof myDate);
+
+let createDate= new Date(23,0,5,3,4);
+console.log(createDate.toLocaleString());
